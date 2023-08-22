e0e14a07dc30bde24cab037a648a01fe8bf7c70f
Nishad Sheriffdeen <nishadsheriffdeen@gmail.com>
2023-08-23 00:22:16 +0530

    feat: user model class

---
+++ src/main/java/com/codewithnishad/consultantwebsite/model/User.java
@@ -0,0 +1,96 @@
+package com.codewithnishad.consultantwebsite.model;
+
+public class User {
+
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String gender;
+	private String role;
+	private String email;
+	private String password;
+	private String phoneNumber;
+
+	public User() {
+
+	}
+
+	public User(String firstName, String lastName, int age, String gender, String role, String email, String password,
+			String phoneNumber) {
+
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.gender = gender;
+		this.role = role;
+		this.email = email;
+		this.password = password;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
+
